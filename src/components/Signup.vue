926e4fa923b26dd8bd6e3f64d4796cab85c3beab
Corey Lanier <iam@coreylanier.com>
2021-03-18 23:00:11 -0400

    changed user creation flow

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -42,17 +42,15 @@ export default {
     doSignup: function () {
       const { email, password, verifyPassword, username } = this.$data;
       if (password === verifyPassword) {
-        auth
-          .createUserWithEmailAndPassword(email, password)
-          .then((res) => {
-            const dbRef = firebase.database().ref(`users/${res.user.uid}`);
-            dbRef.set({ username, email, isAdmin: false });
-            window.location.assign("/");
-          })
-          .catch((err) => {
-            console.error(err.code, err.message);
-            this.error = err.message;
-          });
+        auth.createUserWithEmailAndPassword(email, password).catch((err) => {
+          console.error(err.code, err.message);
+          this.error = err.message;
+        });
+        auth.onAuthStateChanged((user) => {
+          const dbRef = firebase.database().ref(`users/${user.uid}`);
+          dbRef.set({ username, email, isAdmin: false });
+          window.location.assign("/");
+        });
       } else {
         console.error("Passwords don't match.");
         this.error = "Passwords don't match.";
